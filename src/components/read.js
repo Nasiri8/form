2bc8754f34703d2ee9cc2db19663fb0dce265944
nasiri <samiramis.nasiri79@gmail.com>
2021-12-27 12:01:11 +0330

    new

---
+++ src/components/read.js
@@ -0,0 +1,79 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Table, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+export default function Read() {
+    const [APIData, setAPIData] = useState([]);
+    useEffect(() => {
+        axios.get(`https://60fbca4591156a0017b4c8a7.mockapi.io/fakeData`)
+            .then((response) => {
+                console.log(response.data)
+                setAPIData(response.data);
+            })
+    }, []);
+
+    const setData = (data) => {
+        let { id, firstName, lastName,NationalCode,UniversityUnit, checkbox } = data;
+        localStorage.setItem('ID', id);
+        localStorage.setItem('First Name', firstName);
+        localStorage.setItem('Last Name', lastName);
+        localStorage.setItem('National Code', NationalCode);
+        localStorage.setItem('University Unit', UniversityUnit);
+        localStorage.setItem('Checkbox Value', checkbox)
+    }
+
+    const getData = () => {
+        axios.get(`https://60fbca4591156a0017b4c8a7.mockapi.io/fakeData`)
+            .then((getData) => {
+                setAPIData(getData.data);
+            })
+    }
+
+    const onDelete = (id) => {
+        axios.delete(`https://60fbca4591156a0017b4c8a7.mockapi.io/fakeData/${id}`)
+        .then(() => {
+            getData();
+        })
+    }
+
+    return (
+        <div>
+            <Table singleLine>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>First Name</Table.HeaderCell>
+                        <Table.HeaderCell>Last Name</Table.HeaderCell>
+                        <Table.HeaderCell>National Code</Table.HeaderCell>
+                        <Table.HeaderCell>University Unit</Table.HeaderCell>
+                        <Table.HeaderCell>Checkbox Value</Table.HeaderCell>
+                        <Table.HeaderCell>Update</Table.HeaderCell>
+                        <Table.HeaderCell>Delete</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    {APIData.map((data) => {
+                        return (
+                            <Table.Row>
+                                <Table.Cell>{data.firstName}</Table.Cell>
+                                <Table.Cell>{data.lastName}</Table.Cell>
+                                <Table.Cell>{data.NationalCode}</Table.Cell>
+                                <Table.Cell>{data.UniversityUnit}</Table.Cell>
+                                <Table.Cell>{data.checkbox ? 'Checked' : 'Unchecked'}</Table.Cell>
+                                <Link to='/update'>
+                                    <Table.Cell> 
+                                        <Button onClick={() => setData(data)}>Update</Button>
+                                    </Table.Cell>
+                                </Link>
+                                <Table.Cell>
+                                    <Button onClick={() => onDelete(data.id)}>Delete</Button>
+                                </Table.Cell>
+                            </Table.Row>
+                        )
+                    })}
+                </Table.Body>
+            </Table>
+        </div>
+    )
+}
---
+++ src/components/update.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+import axios from 'axios';
+import { useHistory } from 'react-router';
+
+export default function Update() {
+    let history = useHistory();
+    const [id, setID] = useState(null);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [NationalCode, setNationalCode] = useState('');
+    const [UniversityUnit, setUniversityUnit] = useState('');
+    const [checkbox, setCheckbox] = useState(false);
+
+    useEffect(() => {
+        setID(localStorage.getItem('ID'))
+        setFirstName(localStorage.getItem('First Name'));
+        setLastName(localStorage.getItem('Last Name'));
+        setNationalCode(localStorage.getItem('National Code'));
+        setUniversityUnit(localStorage.getItem('University Unit'));
+        setCheckbox(localStorage.getItem('Checkbox Value'));
+    }, []);
+
+    const updateAPIData = () => {
+        axios.put(`https://60fbca4591156a0017b4c8a7.mockapi.io/fakeData/${id}`, {
+            firstName,
+            lastName,
+            NationalCode,
+            UniversityUnit,
+            checkbox
+        }).then(() => {
+            history.push('/read')
+        })
+    }
+    return (
+        <div>
+            <Form className="create-form">
+                <Form.Field>
+                    <label>First Name</label>
+                    <input placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>National Code</label>
+                    <input placeholder='National Code' value={NationalCode} onChange={(e) => setNationalCode(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>University Unit</label>
+                    <input placeholder='University Unit' value={UniversityUnit} onChange={(e) => setUniversityUnit(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <Checkbox label='I agree to the Terms and Conditions' checked={checkbox} onChange={() => setCheckbox(!checkbox)}/>
+                </Form.Field>
+                <Button type='submit' onClick={updateAPIData}>Update</Button>
+            </Form>
+        </div>
+    )
+}
